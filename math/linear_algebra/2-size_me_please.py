24c90e6cb1f7f5c2f8431b882e28486f3ed790a7
gallit0 <mateogesede@gmail.com>
2023-03-21 00:22:46 -0300

    task 2 done

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -5,7 +5,14 @@ measures the shape of an array by
 returning an array with these values
 """
 
+
 def matrix_shape(matrix):
+    """
+    Recursive function that
+    returns a list of numbers
+    that indicate the shape
+    of any given matrix
+    """
     shape = []
     if type(matrix[0]) != list:
         return [len(matrix)]
