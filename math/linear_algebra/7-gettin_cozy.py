1335388ae1502a8119d5069ce0b72f56fd3a40ef
gallit0 <mateogesede@gmail.com>
2023-03-21 13:05:46 -0300

    edge case fixed

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -6,18 +6,9 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """
     Concatenates two matrices along a specific axis
     """
-    if len(mat1) != len(mat2):
-        return None
-    for i in mat1:
-        for j in mat2:
-            if len(i) != len(j):
-                return None
     if axis == 0:
         return mat1 + mat2
     new = []
-    try:
-        for i in range(len(mat1)):
-            new.append(mat1[i] + mat2[i])
-    except Exception:
-        return None
+    for i in range(len(mat1)):
+        new.append(mat1[i] + mat2[i])
     return new
