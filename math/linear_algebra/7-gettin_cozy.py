d2c74234802b2de4192d9ad98f459f2fc171a85c
gallit0 <mateogesede@gmail.com>
2023-03-21 14:37:01 -0300

    task 7 done

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -8,6 +8,6 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """
     if axis == 0 and len(mat1[0]) == len(mat2[0]):
         return mat1 + mat2
-    if len(mat1) == len(mat2):
+    if axis == 1 and len(mat1) == len(mat2):
         return [mat1[i] + mat2[i] for i in range(len(mat2))]
     return None
