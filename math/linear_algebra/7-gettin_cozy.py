f5f836d897ab17c8cbd6ddf6dcd962c1c07654ba
gallit0 <mateogesede@gmail.com>
2023-03-21 12:54:22 -0300

    task 7 done

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -10,9 +10,9 @@ def cat_matrices2D(mat1, mat2, axis=0):
         return mat1 + mat2
     new = []
     if mat1 < mat2:
-        length = mat2
+        length = len(mat2)
     else:
-        length = mat1
+        length = len(mat1)
     for i in range(length):
         new.append(mat1[i] + mat2[i])
     return new
