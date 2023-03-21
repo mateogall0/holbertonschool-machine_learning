97f448d51afe1766f26bce3746e0420daa60a8d6
gallit0 <mateogesede@gmail.com>
2023-03-21 14:52:23 -0300

    task 8 done

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -6,6 +6,8 @@ def mat_mul(mat1, mat2):
     """
     Performs matrix multiplication
     """
+    if len(mat1[0]) != len(mat2):
+        return None
     p = len(mat2[0])
     new = [[0]*p for _ in mat1]
     for i in range(len(mat1)):
