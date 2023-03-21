0f73c57910976789916b52f02051f6dcff77a112
gallit0 <mateogesede@gmail.com>
2023-03-21 02:08:37 -0300

    task 5 done

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -11,7 +11,7 @@ def add_matrices2D(mat1, mat2):
     if len(mat1[0]) == 0 or (len(mat1) != len(mat2)):
         return None
     a = [[] for i in range(len(mat1))]
-    for x in range(len(mat1)):
-        for y in range(len(mat2)):
-            a.append(mat1[x][y] + mat2[x][y])
-    return [a[:-2], a[2:]]
+    for x in range(len(a)):
+        for y in range(len(mat2[x])):
+            a[x].append(mat1[x][y] + mat2[x][y])
+    return a
