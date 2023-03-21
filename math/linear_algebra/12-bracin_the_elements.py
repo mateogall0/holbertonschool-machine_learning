f795ccd889b3ee80a806ca65ac81bf27be91b80c
gallit0 <mateogesede@gmail.com>
2023-03-21 15:31:45 -0300

    task 12 done

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -2,16 +2,9 @@
 """Module"""
 
 
-import numpy as np
-
 def np_elementwise(mat1, mat2):
     """
     performs element-wise addition,
     subtraction, multiplication, and division
     """
-    add = np.add(mat1, mat2)
-    sub = np.subtract(mat1, mat2)
-    mul = np.multiply(mat1, mat2)
-    div = np.divide(mat1, mat2)
-
-    return add, sub, mul, div
+    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
