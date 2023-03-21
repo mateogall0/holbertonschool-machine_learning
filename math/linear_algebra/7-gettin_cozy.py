b1647165c296f9742153e5d0dd08994226082178
gallit0 <mateogesede@gmail.com>
2023-03-21 14:35:53 -0300

    task 7 done

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -6,25 +6,8 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """
     Concatenates two matrices along a specific axis
     """
-    if matrix_shape(mat1) != matrix_shape(mat2):
-        return None
-    if axis == 0:
+    if axis == 0 and len(mat1[0]) == len(mat2[0]):
         return mat1 + mat2
-    new = []
-    for i in range(len(mat1)):
-        new.append(mat1[i] + mat2[i])
-    return new
-
-def matrix_shape(matrix):
-    """
-    Recursive function that
-    returns a list of numbers
-    that indicate the shape
-    of any given matrix
-    """
-    shape = []
-    if type(matrix[0]) != list:
-        return [len(matrix)]
-    shape += [len(matrix)]
-    shape += matrix_shape(matrix[0])
-    return shape
+    if len(mat1) == len(mat2):
+        return [mat1[i] + mat2[i] for i in range(len(mat1))]
+    return None
