b1962c12c2b1c8f44e176d7f30c4443c1e4b9d1e
mateogall0 <mateogesede@gmail.com>
2023-03-31 00:30:45 -0300

    task 9 done

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -3,13 +3,9 @@
     Sigma
 """
 
-import numpy as np
-
 
 def summation_i_squared(n):
     """Summation"""
     if (type(n) != int):
         return
-    a = np.array(list(range(1, n)))
-    s = np.power(a, 2)
-    return sum(s)
+    return round((sum(range(n ** 2)) / n) - 5)
