e5a7b9ff743adea9936b15407789f47bcb8beb01
mateogall0 <mateogesede@gmail.com>
2023-03-31 11:52:04 -0300

    task 10 done

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""calculates the derivative of a polynomial"""
+
+
+def poly_derivative(poly):
+    """Calculate the derivative of a polynomial"""
+    result = []
+    if type(poly) != list and type(poly) != tuple:
+        return None
+    if len(poly) == 1:
+        return [0]
+    for idx, i in enumerate(poly):
+        if type(i) != int:
+            return None
+        if idx == 0:
+            continue
+        result.append(i * idx)
+
+    return result
