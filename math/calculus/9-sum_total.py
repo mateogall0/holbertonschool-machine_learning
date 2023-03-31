bbe8a0b4145d3e3b3f9fa9b01ac68082116f6000
mateogall0 <mateogesede@gmail.com>
2023-03-31 00:50:25 -0300

    task 9 done

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -8,4 +8,4 @@ def summation_i_squared(n):
     """Summation"""
     if (type(n) != int):
         return
-    return round((sum(range(n ** 2)) / n) - n)
+    return int(n * (n + 1) * (2 * n + 1) / 6)
