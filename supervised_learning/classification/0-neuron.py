08aba54124d93c1b12ae7d7914d094183a6f0dba
gallit0 <mateogesede@gmail.com>
2023-04-11 09:50:37 -0300

    task 0 done

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,22 @@
+#!/usr/bin/end python3
+"""
+    Module Neuron
+"""
+
+
+import numpy as np
+
+
+class Neuron:
+    """
+        Neuron class
+    """
+
+    def __init__(self, nx):
+        if type(nx) != int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.W = np.random.normal(size=(nx,))
+        self.b = 0
+        self.A = 0
