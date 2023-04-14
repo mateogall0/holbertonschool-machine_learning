50747ee76b3e16898126cdf040d4e1c2ca9a71e5
mateogall0 <mateogesede@gmail.com>
2023-04-14 11:18:43 -0300

    task 12

--- supervised_learning/classification/12-neural_network.py
+++ supervised_learning/classification/12-neural_network.py
@@ -82,6 +82,6 @@ class NeuralNetwork:
         """
         A1, A2 = self.forward_prop(X)
         A = A1 * A2
-        prediction = np.where((A >= 0.5), 1, 0).reshape(1, -1)
+        prediction = np.where((A >= 0.5), 1, 0)
         cost2 = self.cost(Y, A2)
-        return prediction, cost2
+        return prediction[-1:], cost2
