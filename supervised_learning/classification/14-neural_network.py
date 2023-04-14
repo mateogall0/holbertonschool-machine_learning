a6fb931f1dd8a606a03df0cf84cd4b6afeda9c30
mateogall0 <mateogesede@gmail.com>
2023-04-14 15:36:19 -0300

    task 14 done

---
+++ supervised_learning/classification/14-neural_network.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+"""
+    defines a neural network with one hidden
+    layer performing binary classification
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class NeuralNetwork:
+    """
+        Neural Network class
+    """
+    def __init__(self, nx, nodes):
+        if type(nx) != int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if type(nodes) != int:
+            raise TypeError('nodes must be an integer')
+        if nodes < 1:
+            raise ValueError('nodes must be a positive integer')
+        self.__W1 = np.random.normal(size=(nodes, nx))
+        self.__b1 = np.zeros(shape=(nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.normal(size=(1, nodes))
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        return self.__W1
+
+    @property
+    def b1(self):
+        return self.__b1
+
+    @property
+    def A1(self):
+        return self.__A1
+
+    @property
+    def W2(self):
+        return self.__W2
+
+    @property
+    def b2(self):
+        return self.__b2
+
+    @property
+    def A2(self):
+        return self.__A2
+
+    def sigmoid(self, x):
+        """
+            Sigmoid activation function
+        """
+        return 1 / (1 + np.exp(-x))
+
+    def forward_prop(self, X):
+        """
+            Calculates the forward propagation of the neurons
+        """
+        z1 = np.dot(self.W1, X) + self.b1
+        self.__A1 = self.sigmoid(z1)
+
+        z2 = np.dot(self.W2, self.A1) + self.b2
+        self.__A2 = self.sigmoid(z2)
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """
+            Calculates the cost of the model using logistic regression
+        """
+        m = Y.shape[1]
+        return -(np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))) / m
+
+    def evaluate(self, X, Y):
+        """
+            Evaluates the neural network's predictions
+        """
+        A1, A2 = self.forward_prop(X)
+        prediction = np.where((A2 > 0.5), 1, 0)
+        cost2 = self.cost(Y, A2)
+        return prediction, cost2
+
+    def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
+        """
+            Calculates one pass of gradient descent on the neural network
+        """
+        m = Y.shape[1]
+        dz2 = A2 - Y
+        dw2 = np.matmul(dz2, A1.T) / m
+        db2 = np.sum(dz2, axis=1, keepdims=True) / m
+        dz1 = np.matmul(self.W2.T, dz2) * (A1 * (1 - A1))
+        dw1 = np.matmul(dz1, X.T) / m
+        db1 = np.sum(dz1, axis=1, keepdims=True) / m
+        self.__W2 -= alpha * dw2
+        self.__b2 -= alpha * db2
+        self.__W1 -= alpha * dw1
+        self.__b1 -= alpha * db1
+
+    def train(self, X, Y, iterations=5000, alpha=0.05, verbose=True, graph=True, step=100):
+        """
+            Trains the neural network
+        """
+        if type(iterations) != int:
+            raise TypeError('iterations must be an integer')
+        if iterations < 0:
+            raise ValueError('iterations must be a positive integer')
+        if type(alpha) != float:
+            raise TypeError('alpha must be a float')
+        if alpha < 0.0:
+            raise ValueError('alpha must be positive')
+        if verbose or graph:
+            if type(step) != int:
+                raise TypeError('step must be an integer')
+            if step <= 0 or step > iterations:
+                raise ValueError('step must be positive and <= iterations')
+
+        data = []
+
+        for i in range(iterations + 1):
+            A1, A2 = self.forward_prop(X)
+            cost = self.cost(Y, A1)
+            self.gradient_descent(X, Y, A1, A2, alpha)
+            if verbose and (i % step == 0 or i % iterations == 0):
+                data.append({i: cost})
+                print(f'Cost after {i} iterations: {cost}')
+        if graph and len(data) > 0:
+            x = [list(d.keys())[0] for d in data]
+            y = [list(d.values())[0] for d in data]
+            plt.plot(x, y)
+            plt.xlabel('iteration')
+            plt.ylabel('cost')
+            plt.title('Training Cost')
+            plt.show()
+        return self.evaluate(X, Y)
