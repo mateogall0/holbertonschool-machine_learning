44f3be50e2aff418edd2248ab1a95a838926f95f
mateogall0 <mateogesede@gmail.com>
2023-04-16 19:46:32 -0300

    task 16 done

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -27,9 +27,9 @@ class DeepNeuralNetwork:
         for i in range(1, self.L + 1):
             if i == 1:
                 self.weights["W" + str(i)] = \
-                    np.random.normal(layers[i - 1], nx) * np.sqrt(2 / nx)
+                    np.random.randn(layers[i - 1], nx) * np.sqrt(2 / nx)
             else:
                 self.weights["W" + str(i)] = \
-                    np.random.normal(layers[i - 1], layers[i - 2]) * \
+                    np.random.randn(layers[i - 1], layers[i - 2]) * \
                     np.sqrt(2 / layers[i - 2])
             self.weights["b" + str(i)] = np.zeros((layers[i - 1], 1))
