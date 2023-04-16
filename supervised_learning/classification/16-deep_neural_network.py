03c8593d89c14cbf82ed7d9101335759c95dd7de
mateogall0 <mateogesede@gmail.com>
2023-04-16 19:38:41 -0300

    task 16

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -18,7 +18,7 @@ class DeepNeuralNetwork:
             raise ValueError('nx must be a positive integer')
         if type(layers) != list or len(layers) == 0:
             raise ValueError('layers must be a list of positive integers')
-        if not all(isinstance(x, int) for x in layers):
+        if not all(map(lambda item: isinstance(item, int), layers)):
             raise TypeError('layers must be a list of positive integers')
         self.L = len(layers)
         self.cache = {}
