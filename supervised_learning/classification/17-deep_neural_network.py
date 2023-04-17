5fcadd5585af6f9dd4e91a22e736d4962ed5ce5a
mateogall0 <mateogesede@gmail.com>
2023-04-17 00:47:15 -0300

    task 17 done

---
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+    defines a deep neural network performing binary classification
+"""
+
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """
+        Deep Neural Network class
+    """
+    def __init__(self, nx, layers):
+        if type(nx) != int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if type(layers) != list or len(layers) == 0:
+            raise TypeError('layers must be a list of positive integers')
+        if not all(map(lambda item: isinstance(item, int), layers)):
+            raise ValueError('layers must be a list of positive integers')
+        if not all(map(lambda item: item >= 0, layers)):
+            raise TypeError('layers must be a list of positive integers')
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+
+        for i in range(1, self.L + 1):
+            if i == 1:
+                self.__weights["W" + str(i)] = \
+                    np.random.randn(layers[i - 1], nx) * np.sqrt(2 / nx)
+            else:
+                self.__weights["W" + str(i)] = \
+                    np.random.randn(layers[i - 1], layers[i - 2]) * \
+                    np.sqrt(2 / layers[i - 2])
+            self.__weights["b" + str(i)] = np.zeros((layers[i - 1], 1))
+
+    @property
+    def L(self):
+        return self.__L
+    
+    @property
+    def cache(self):
+        return self.__cache
+    
+    @property
+    def weights(self):
+        return self.__weights
