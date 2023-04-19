0ab6c3d619d3864e8ec5ee832f1bd7c811dc5489
mateogall0 <mateogesede@gmail.com>
2023-04-19 11:06:42 -0700

    attempt

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -10,7 +10,6 @@ import tensorflow as tf
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 calculate_loss = __import__('4-calculate_loss').calculate_loss
 create_placeholders = __import__('0-create_placeholders').create_placeholders
-create_layer = __import__('1-create_layer').create_layer
 create_train_op = __import__('5-create_train_op').create_train_op
 forward_prop = __import__('2-forward_prop').forward_prop
 
