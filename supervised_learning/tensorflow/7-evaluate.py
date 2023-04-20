d8eb0a5ce87fe0bf23e896680f543ede3ea15de8
mateogall0 <mateogesede@gmail.com>
2023-04-20 06:58:03 -0700

    attempt

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -12,18 +12,19 @@ def evaluate(X, Y, save_path):
         Evaluates the output of a neural network
     """
     graph = tf.Graph()
-    with graph.as_default():
-        sess = tf.Session()
-        saver = tf.train.import_meta_graph(save_path + '.meta')
+    with tf.compat.v1.Session(graph=graph) as sess:
+        saver = tf.compat.v1.train.import_meta_graph(save_path + '.meta')
         saver.restore(sess, save_path)
 
-    # Get the required tensors from the graph's collection
-    y_pred = graph.get_tensor_by_name('y_pred:0')
-    loss = graph.get_tensor_by_name('loss:0')
-    accuracy = graph.get_tensor_by_name('accuracy:0')
+        # Get the tensors we need
+        y_pred = graph.get_collection('y_pred')[0]
+        loss = graph.get_collection('loss')[0]
+        accuracy = graph.get_collection('accuracy')[0]
+        X_placeholder = graph.get_collection('X')[0]
+        Y_placeholder = graph.get_collection('Y')[0]
 
-    # Evaluate the model on the given data
-    feed_dict = {'X:0': X, 'Y:0': Y}
-    pred, acc, cost = sess.run([y_pred, accuracy, loss], feed_dict=feed_dict)
+        # Evaluate the model
+        feed_dict = {X_placeholder: X, Y_placeholder: Y}
+        y_pred_val, loss_val, accuracy_val = sess.run([y_pred, loss, accuracy], feed_dict=feed_dict)
 
-    return pred, acc, cost
+    return y_pred_val, accuracy_val, loss_val
