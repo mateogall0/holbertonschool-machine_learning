5a0ca5e227b08c8d6288c91c7b7a303ecfa40226
mateogall0 <mateogesede@gmail.com>
2023-04-20 07:01:41 -0700

    attempt

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -12,19 +12,26 @@ def evaluate(X, Y, save_path):
         Evaluates the output of a neural network
     """
     graph = tf.Graph()
-    with tf.compat.v1.Session(graph=graph) as sess:
-        saver = tf.compat.v1.train.import_meta_graph(save_path + '.meta')
+	with graph.as_default():
+        saver = tf.train.import_meta_graph(save_path + '.meta')
+
+    # Extract the tensors we need from the graph's collection
+    with tf.Session(graph=graph) as sess:
+        # Load the weights from the checkpoint
         saver.restore(sess, save_path)
+        
+        # Get the input and output tensors
+        X_input = graph.get_tensor_by_name('input:0')
+        y_pred = graph.get_tensor_by_name('output:0')
+        
+        # Get the loss tensor
+        loss = tf.get_collection('loss')[0]
+        
+        # Get the accuracy tensor
+        accuracy = tf.get_collection('accuracy')[0]
 
-        # Get the tensors we need
-        y_pred = graph.get_collection('y_pred')[0]
-        loss = graph.get_collection('loss')[0]
-        accuracy = graph.get_collection('accuracy')[0]
-        X_placeholder = graph.get_collection('X')[0]
-        Y_placeholder = graph.get_collection('Y')[0]
+        # Evaluate the network
+        feed_dict = {X_input: X, y_true: Y}
+        y_pred_val, loss_val, acc_val = sess.run([y_pred, loss, accuracy], feed_dict=feed_dict)
 
-        # Evaluate the model
-        feed_dict = {X_placeholder: X, Y_placeholder: Y}
-        y_pred_val, loss_val, accuracy_val = sess.run([y_pred, loss, accuracy], feed_dict=feed_dict)
-
-    return y_pred_val, accuracy_val, loss_val
+    return y_pred_val, acc_val, loss_val
