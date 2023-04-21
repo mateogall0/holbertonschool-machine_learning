8ac3862bdc717797a7694b0ef37f9213df26e7c7
Mateo Gallo <92031297+mateogall0@users.noreply.github.com>
2023-04-20 22:13:47 -0300

    Update 7-evaluate.py

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -12,18 +12,16 @@ def evaluate(X, Y, save_path):
         Evaluates the output of a neural network
     """
     with tf.Session() as sess:
-        # Load the saved graph and restore the variables
-        saver = tf.train.import_meta_graph(save_path)
+        saver = tf.train.import_meta_graph(save_path + ".meta")
         saver.restore(sess, save_path)
 
-        # Get the required tensors from the graph's collection
-        graph = tf.get_default_graph()
-        y_pred = graph.get_tensor_by_name('y_pred:0')
-        loss = graph.get_collection('loss')[0]
-        accuracy = graph.get_collection('accuracy')[0]
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[0]
+        y_pred = tf.get_collection("y_pred")[0]
+        accuracy = tf.get_collection("accuracy")[0]
+        loss = tf.get_collection("loss")[0]
 
-        # Evaluate the network's prediction, accuracy, and loss
-        feed_dict = {graph.get_tensor_by_name('X:0'): X, graph.get_tensor_by_name('Y:0'): Y}
-        y_pred_val, acc_val, loss_val = sess.run([y_pred, accuracy, loss], feed_dict=feed_dict)
-
-    return y_pred_val, acc_val, loss_val
+        y_eval = sess.run(y_pred, feed_dict={x: X, y: Y})
+        accuracy_eval = sess.run(accuracy, feed_dict={x: X, y: Y})
+        loss_eval = sess.run(loss, feed_dict={x: X, y: Y})
+        return y_eval, accuracy_eval, loss_eval
