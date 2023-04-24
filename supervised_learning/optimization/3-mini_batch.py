41b9ed6559e28da09840f3b23ca22307d32e5122
gallit0 <mateogesede@gmail.com>
2023-04-24 10:30:29 -0300

    attempt

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -20,13 +20,13 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid,
 
     # Load the model graph and restore the session
     saver = tf.train.import_meta_graph(load_path + '.meta')
-    x = tf.get_collection('x')[0]
-    y = tf.get_collection('y')[0]
-    accuracy = tf.get_collection('accuracy')[0]
-    loss = tf.get_collection('loss')[0]
-    train_op = tf.get_collection('train_op')[0]
+    x = tf.compat.v1.get_collection('x')[0]
+    y = tf.compat.v1.get_collection('y')[0]
+    accuracy = tf.compat.v1.get_collection('accuracy')[0]
+    loss = tf.compat.v1.get_collection('loss')[0]
+    train_op = tf.compat.v1.get_collection('train_op')[0]
     
-    with tf.Session() as sess:
+    with tf.compat.v1.Session() as sess:
         # Restore the trained model
         saver.restore(sess, load_path)
         
@@ -35,6 +35,14 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid,
             # Shuffle the training data
             X_train, Y_train = shuffle_data(X_train, Y_train)
             
+            # Calculate training and validation cost and accuracy after each epoch
+            train_cost, train_accuracy = sess.run([loss, accuracy], feed_dict={x: X_train, y: Y_train})
+            valid_cost, valid_accuracy = sess.run([loss, accuracy], feed_dict={x: X_valid, y: Y_valid})
+            
+            # Print progress after each epoch
+            print("After {} epochs:".format(epoch+1))
+            print("\tTraining Cost = {}\n\tTraining Accuracy = {}".format(train_cost, train_accuracy))
+            print("\tValidation Cost = {}\n\tValidation Accuracy = {}".format(valid_cost, valid_accuracy))
             # Loop over batches
             for i in range(0, X_train.shape[0], batch_size):
                 # Get the next batch of data
@@ -48,15 +56,7 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid,
                 if i != 0 and i % 100 == 0:
                     print("\tStep {}:\n\t\tCost: {}\n\t\tAccuracy: {}".format(i, step_cost, step_accuracy))
             
-            # Calculate training and validation cost and accuracy after each epoch
-            train_cost, train_accuracy = sess.run([loss, accuracy], feed_dict={x: X_train, y: Y_train})
-            valid_cost, valid_accuracy = sess.run([loss, accuracy], feed_dict={x: X_valid, y: Y_valid})
-            
-            # Print progress after each epoch
-            print("After {} epochs:".format(epoch+1))
-            print("\tTraining Cost = {}\n\tTraining Accuracy = {}".format(train_cost, train_accuracy))
-            print("\tValidation Cost = {}\n\tValidation Accuracy = {}".format(valid_cost, valid_accuracy))
-        
+
         # Save the trained model
         save_path = saver.save(sess, save_path)
         
