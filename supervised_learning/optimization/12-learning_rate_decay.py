6870eccde2b0da678d14a6ffb1ae71aa84f86ea3
gallit0 <mateogesede@gmail.com>
2023-04-27 12:41:52 -0300

    task 12

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -19,4 +19,10 @@ def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
         decay_step -- number of passes of gradient descent that
         should occur before alpha is decayed further
     """
-    return tf.train.inverse_time_decay(alpha, global_step, decay_step, decay_rate)
+    return tf.train.inverse_time_decay(
+        alpha,
+        global_step=global_step,
+        decay_steps=decay_step,
+        decay_rate=decay_rate,
+        staircase=True
+    )
