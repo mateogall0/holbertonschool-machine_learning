c52758b18dd1176b73afb94dfc83848d60203470
gallit0 <mateogesede@gmail.com>
2023-04-27 14:39:24 -0300

    attempt

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -17,18 +17,21 @@ def create_batch_norm_layer(prev, n, activation):
         activation -- activation function that should
         be used on the output of the layer
     """
-    # Using tf.layers.Dense as the base layer with variance_scaling_initializer
-    layer = tf.layers.Dense(units=n, kernel_initializer=tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG"))
+    # Layers
+    k_init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    output = tf.layers.Dense(units=n, kernel_initializer=k_init)
+    Z = output(prev)
 
-    # Creating gamma and beta trainable parameters initialized as vectors of 1 and 0 respectively
-    gamma = tf.Variable(initial_value=tf.ones([n]), name='gamma')
-    beta = tf.Variable(initial_value=tf.zeros([n]), name='beta')
+    # Gamma and Beta initialization
+    gamma = tf.Variable(initial_value=tf.constant(1.0, shape=[n]),
+                        name="gamma")
+    beta = tf.Variable(initial_value=tf.constant(0.0, shape=[n]), name="beta")
 
-    # Calculating the mean and variance of the layer inputs
-    mean, variance = tf.nn.moments(prev, axes=[0])
-
-    # Creating the batch normalization layer
-    batch_norm = tf.nn.batch_normalization(prev, mean, variance, beta, gamma, epsilon=1e-8)
-
-    # Applying activation function to the output of batch normalization layer
-    return activation(batch_norm)
+    # Batch normalization
+    mean, var = tf.nn.moments(Z, axes=0)
+    b_norm = tf.nn.batch_normalization(Z, mean, var, offset=beta,
+                                       scale=gamma,
+                                       variance_epsilon=1e-8)
+    if activation is None:
+        return b_norm
+    return activation(b_norm)
