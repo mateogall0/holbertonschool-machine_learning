a6bbe4cbaff210fa0fe32f608b9b3919d155c060
gallit0 <mateogesede@gmail.com>
2023-04-27 12:43:32 -0300

    staircase

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -18,6 +18,20 @@ def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
         global_step -- number of passes of gradient descent that have elapsed
         decay_step -- number of passes of gradient descent that
         should occur before alpha is decayed further
+
+
+
+        If staircase=True, the learning rate will decay in a stepwise
+        fashion, meaning that the learning rate will be reduced by a
+        factor of decay_rate every decay_step steps. This is useful
+        when you want to apply a larger decay to the learning rate at
+        specific intervals, such as at the end of an epoch or after a
+        certain number of steps.
+
+        If staircase=False, the learning rate will decay continuously,
+        meaning that the learning rate will be decayed by a factor of
+        decay_rate for every step, which can be useful when you want a
+        more gradual and continuous decay.
     """
     return tf.train.inverse_time_decay(
         alpha,
