a387938c89465b2bad468602191f33a2bffceb4a
mateogall0 <mateogesede@gmail.com>
2023-04-29 00:17:12 -0300

    attempt

--- supervised_learning/optimization/15-model.py
+++ supervised_learning/optimization/15-model.py
@@ -229,54 +229,81 @@ def model(Data_train, Data_valid, layers, activations,
         pass through the whole dataset
         save_path -- path where the model should be saved to
     """
-    X_train, Y_train = Data_train
-    X_valid, Y_valid = Data_valid
-    x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
+    nx = Data_train[0].shape[1]
+    classes = Data_train[1].shape[1]
+
+    (X_train, Y_train) = Data_train
+    (X_valid, Y_valid) = Data_valid
+
+    x = tf.placeholder(tf.float32, shape=[None, nx], name='x')
+    y = tf.placeholder(tf.float32, shape=[None, classes], name='y')
+
     tf.add_to_collection('x', x)
     tf.add_to_collection('y', y)
+
     y_pred = forward_prop(x, layers, activations)
     tf.add_to_collection('y_pred', y_pred)
-    accuracy = calculate_accuracy(y, y_pred)
-    tf.add_to_collection('accuracy', accuracy)
+
     loss = calculate_loss(y, y_pred)
     tf.add_to_collection('loss', loss)
+
+    accuracy = calculate_accuracy(y, y_pred)
+    tf.add_to_collection('accuracy', accuracy)
+
     global_step = tf.Variable(0)
-    decay = learning_rate_decay(alpha, decay_rate, global_step, 1)
-    train_op = create_Adam_op(loss, decay, beta1, beta2, epsilon)
+    alpha_op = learning_rate_decay(alpha, decay_rate, global_step, 1)
+    train_op = create_Adam_op(loss, alpha_op, beta1, beta2, epsilon)
     tf.add_to_collection('train_op', train_op)
+
     init = tf.global_variables_initializer()
     saver = tf.train.Saver()
 
     with tf.Session() as sess:
         sess.run(init)
-        size = X_train.shape[0] // batch_size
-        if X_train.shape[0] % batch_size != 0:
-            size += 1
-        for i in range(epochs + 1):
-            cost_t, acc_t = sess.run([loss, accuracy],
-                                     feed_dict={x: X_train, y: Y_train})
-            cost_v, acc_v = sess.run([loss, accuracy],
-                                     feed_dict={x: X_valid, y: Y_valid})
-            print("After {} epochs:".format(i))
-            print("\tTraining Cost: {}".format(cost_t))
-            print("\tTraining Accuracy: {}".format(acc_t))
-            print("\tValidation Cost: {}".format(cost_v))
-            print("\tValidation Accuracy: {}".format(acc_v))
-            if i < epochs:
-                x_sh, y_sh = shuffle_data(X_train, Y_train)
-                for j in range(size):
-                    start = j * batch_size
-                    end = start + batch_size
-                    if end > x_sh.shape[0]:
-                        end = x_sh.shape[0]
-                    x_mini = x_sh[start:end]
-                    y_mini = y_sh[start:end]
-                    sess.run(train_op, feed_dict={x: x_mini, y: y_mini})
-                    if (j + 1) % 100 == 0 and j > 0:
-                        cost, acc = sess.run([loss, accuracy],
-                                             feed_dict={x: x_mini, y: y_mini})
-                        print("\tStep {}:".format(j + 1))
-                        print("\t\tCost: {}".format(cost))
-                        print("\t\tAccuracy: {}".format(acc))
+        m = X_train.shape[0]
+        if (m % batch_size) == 0:
+            num_minibatches = int(m / batch_size)
+            check = 1
+        else:
+            num_minibatches = int(m / batch_size) + 1
+            check = 0
+
+        for epoch in range(epochs + 1):
+            feed_train = {x: X_train, y: Y_train}
+            feed_valid = {x: X_valid, y: Y_valid}
+            train_cost = sess.run(loss, feed_dict=feed_train)
+            train_accuracy = sess.run(accuracy, feed_dict=feed_train)
+            valid_cost = sess.run(loss, feed_dict=feed_valid)
+            valid_accuracy = sess.run(accuracy, feed_dict=feed_valid)
+
+            print("After {} epochs:".format(epoch))
+            print("\tTraining Cost: {}".format(train_cost))
+            print("\tTraining Accuracy: {}".format(train_accuracy))
+            print("\tValidation Cost: {}".format(valid_cost))
+            print("\tValidation Accuracy: {}".format(valid_accuracy))
+
+            if epoch < epochs:
+                Xs, Ys = shuffle_data(X_train, Y_train)
+
+                for step_number in range(num_minibatches):
+                    start = step_number * batch_size
+                    end = (step_number + 1) * batch_size
+                    if check == 0 and step_number == num_minibatches - 1:
+                        x_minbatch = Xs[start::]
+                        y_minbatch = Ys[start::]
+                    else:
+                        x_minbatch = Xs[start:end]
+                        y_minbatch = Ys[start:end]
+
+                    feed_mini = {x: x_minbatch, y: y_minbatch}
+                    sess.run(train_op, feed_dict=feed_mini)
+
+                    if ((step_number + 1) % 100 == 0) and (step_number != 0):
+                        step_cost = sess.run(loss, feed_dict=feed_mini)
+                        step_accuracy = sess.run(accuracy, feed_dict=feed_mini)
+                        print("\tStep {}:".format(step_number + 1))
+                        print("\t\tCost: {}".format(step_cost))
+                        print("\t\tAccuracy: {}".format(step_accuracy))
             sess.run(tf.assign(global_step, global_step + 1))
-        return saver.save(sess, save_path)
+            save_path = saver.save(sess, save_path)
+    return save_path
