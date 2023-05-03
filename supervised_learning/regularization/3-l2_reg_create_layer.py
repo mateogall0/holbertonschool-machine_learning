cc11e0f3245208df6fa1aae8c7c5ed301a231927
gallit0 <mateogesede@gmail.com>
2023-05-03 12:34:46 -0300

    attempt

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -18,17 +18,17 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
         Returns: the output of the new layer
     """
     # Create weight matrix for the layer with L2 regularization
-    initializer = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    initializer = tf.contrib.layers.variance_scaling_initializer()
     regularizer = tf.contrib.layers.l2_regularizer(scale=lambtha)
     w = tf.Variable(initializer([int(prev.shape[1]), n]), dtype=tf.float32, name="kernel", regularizer=regularizer)
-    
+
     # Compute the linear transformation of the previous layer
     z = tf.matmul(prev, w)
-    
+
     # Apply the activation function
     if activation is not None:
         a = activation(z)
     else:
         a = z
-    
+
     return a
