c947e095217fb73b5c7bee1c782ca6c3c98552be
gallit0 <mateogesede@gmail.com>
2023-05-03 12:38:38 -0300

    attempt

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -17,18 +17,26 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
         lambtha -- L2 regularization parameter
         Returns: the output of the new layer
     """
-    # Create weight matrix for the layer with L2 regularization
-    initializer = tf.contrib.layers.variance_scaling_initializer()
-    regularizer = tf.contrib.layers.l2_regularizer(scale=lambtha)
-    w = tf.Variable(initializer([int(prev.shape[1]), n]), name="kernel", regularizer=regularizer)
+    # Creates an initializer for the layer's weight matrix using the variance
+    # scaling initializer. The "FAN_AVG" mode is chosen, which is a
+    # recommended mode for initializing weights in deep neural networks.
+    # This initializer helps with maintaining the variance of activations and
+    # gradients throughout the network
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
 
-    # Compute the linear transformation of the previous layer
-    z = tf.matmul(prev, w)
+    # An L2 regularizer is created using the l2_regularizer function from
+    # tf.contrib.layers. The lambtha parameter is used as the
+    # regularization strength. L2 regularization penalizes the model for
+    # having large weight values, helping to prevent overfitting.
+    reg = tf.contrib.layers.l2_regularizer(lambtha)
 
-    # Apply the activation function
-    if activation is not None:
-        a = activation(z)
-    else:
-        a = z
-
-    return a
+    # This line creates a dense (fully connected) layer using the
+    # tf.layers.Dense function. It specifies the number of nodes in the
+    # layer as n, the activation function as activation, the weight
+    # initializer as init, and the weight regularizer as reg. The name
+    # parameter provides a name for the layer.
+    layer = tf.layers.Dense(n, activation=activation,
+                            kernel_initializer=init,
+                            name="layer",
+                            kernel_regularizer=reg)
+    return layer(prev)
