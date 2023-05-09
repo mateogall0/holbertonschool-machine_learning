0688ed334eee2d37f9d3e465e8a29b84aca10e3c
gallit0 <mateogesede@gmail.com>
2023-05-09 16:37:02 -0300

    pycodestyle

--- supervised_learning/keras/8-train.py
+++ supervised_learning/keras/8-train.py
@@ -62,7 +62,9 @@ def train_model(network, data, labels, batch_size, epochs,
                                                            verbose=True))
 
     if validation_data is not None and save_best and filepath:
-        callbacks.append(K.callbacks.ModelCheckpoint(filepath, save_best_only = True))
+        callbacks.append(
+            K.callbacks.ModelCheckpoint(filepath, save_best_only=True)
+            )
 
     if len(callbacks) == 0:
         callbacks = None
