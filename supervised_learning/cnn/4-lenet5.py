61927275f25408affac53efd5f1e5b13f5f15e66
gallit0 <mateogesede@gmail.com>
2023-05-17 16:52:28 -0300

    done

--- supervised_learning/cnn/4-lenet5.py
+++ supervised_learning/cnn/4-lenet5.py
@@ -25,7 +25,7 @@ def lenet5(x, y):
         Fully connected layer with 84 nodes
         Fully connected softmax output layer with 10 nodes
     All layers requiring initialization should initialize their kernels with
-    the he_normal initialization method: 
+    the he_normal initialization method:
     f.contrib.layers.variance_scaling_initializer()
     All hidden layers requiring activation should use the relu activation
     function
@@ -37,41 +37,72 @@ def lenet5(x, y):
         a tensor for the accuracy of the network
     """
     # Convolutional layer 1
-    conv1 = tf.layers.conv2d(inputs=x, filters=6, kernel_size=(5, 5), padding='same',
-                             activation=tf.nn.relu,
-                             kernel_initializer=tf.contrib.layers.variance_scaling_initializer())
+    conv1 = tf.layers.conv2d(
+        inputs=x,
+        filters=6,
+        kernel_size=(5, 5),
+        padding='same',
+        activation=tf.nn.relu,
+        kernel_initializer=tf.contrib.layers.variance_scaling_initializer()
+        )
 
     # Max pooling layer 1
-    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=(2, 2), strides=(2, 2))
+    pool1 = tf.layers.max_pooling2d(
+        inputs=conv1,
+        pool_size=(2, 2),
+        strides=(2, 2)
+        )
 
     # Convolutional layer 2
-    conv2 = tf.layers.conv2d(inputs=pool1, filters=16, kernel_size=(5, 5), padding='valid',
-                             activation=tf.nn.relu,
-                             kernel_initializer=tf.contrib.layers.variance_scaling_initializer())
+    conv2 = tf.layers.conv2d(
+        inputs=pool1,
+        filters=16,
+        kernel_size=(5, 5),
+        padding='valid',
+        activation=tf.nn.relu,
+        kernel_initializer=tf.contrib.layers.variance_scaling_initializer()
+        )
 
     # Max pooling layer 2
-    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=(2, 2), strides=(2, 2))
+    pool2 = tf.layers.max_pooling2d(
+        inputs=conv2,
+        pool_size=(2, 2),
+        strides=(2, 2)
+        )
 
     # Flatten the pool2 output
     flatten = tf.layers.flatten(pool2)
 
     # Fully connected layer 1
-    fc1 = tf.layers.dense(inputs=flatten, units=120, activation=tf.nn.relu,
-                          kernel_initializer=tf.contrib.layers.variance_scaling_initializer())
+    fc1 = tf.layers.dense(
+        inputs=flatten,
+        units=120,
+        activation=tf.nn.relu,
+        kernel_initializer=tf.contrib.layers.variance_scaling_initializer()
+        )
 
     # Fully connected layer 2
-    fc2 = tf.layers.dense(inputs=fc1, units=84, activation=tf.nn.relu,
-                          kernel_initializer=tf.contrib.layers.variance_scaling_initializer())
+    fc2 = tf.layers.dense(
+        inputs=fc1,
+        units=84,
+        activation=tf.nn.relu,
+        kernel_initializer=tf.contrib.layers.variance_scaling_initializer()
+        )
 
     # Output layer
-    logits = tf.layers.dense(inputs=fc2, units=10,
-                             kernel_initializer=tf.contrib.layers.variance_scaling_initializer())
+    logits = tf.layers.dense(
+        inputs=fc2,
+        units=10,
+        kernel_initializer=tf.contrib.layers.variance_scaling_initializer()
+        )
 
     # Softmax activated output
     output = tf.nn.softmax(logits)
 
     # Loss
-    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=logits))
+    loss = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=logits)
+        )
 
     # Accuracy
     correct_prediction = tf.equal(tf.argmax(output, 1), tf.argmax(y, 1))
