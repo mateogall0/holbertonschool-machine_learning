e2cfec33cb164e8e32253effba7bf82777fde8d1
gallit0 <mateogesede@gmail.com>
2023-05-17 14:16:40 -0300

    done

--- supervised_learning/cnn/3-pool_backward.py
+++ supervised_learning/cnn/3-pool_backward.py
@@ -35,7 +35,7 @@ def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
     (dA_prev)
     """
     m, h_new, w_new, c_new = dA.shape
-    m, h_prev, w_prev, c = A_prev.shape
+    m, _, _, c = A_prev.shape
     kh, kw = kernel_shape
     sh, sw = stride
 
