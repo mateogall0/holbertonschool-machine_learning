19da4164be6b74c2f38aa5e89feaa6559b089619
gallit0 <mateogesede@gmail.com>
2023-06-08 20:19:48 -0300

    done

--- supervised_learning/neural_style_transfer/0-neural_style.py
+++ supervised_learning/neural_style_transfer/0-neural_style.py
@@ -42,12 +42,12 @@ shape (h, w, 3)')
         h, w = image.shape[:2]
         max_side = max(h, w)
         scale_factor = 512 / max_side
-        new_h, new_w = int(scale_factor * h), int(scale_factor * w)
-        resized_image = tf.constant(image.astype(np.int32))
+        new_h, new_w = scale_factor * h, scale_factor * w
+        resized_image = tf.constant(image, tf.int32)
         resized_image = tf.expand_dims(resized_image, axis=0)
-        resized_image = tf.image.resize_images(resized_image, [new_h, new_w])
-        resized_image = tf.clip_by_value(
-            resized_image, clip_value_min=0, clip_value_max=255
-            )
+        resized_image = tf.image.resize_images(resized_image, [new_h, new_w], method=tf.image.ResizeMethod.BICUBIC)
         resized_image = resized_image / 255.0
-        return tf.cast(resized_image, tf.float32)
+        resized_image = tf.clip_by_value(
+            resized_image, clip_value_min=0, clip_value_max=1
+            )
+        return resized_image
