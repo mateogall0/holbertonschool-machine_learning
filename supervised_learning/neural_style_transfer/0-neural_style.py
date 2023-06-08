52cd8023229828a27a8d3ffb6ff4968a02cac51f
gallit0 <mateogesede@gmail.com>
2023-06-08 11:26:46 -0300

    attempt

---
+++ supervised_learning/neural_style_transfer/0-neural_style.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+Initialize
+"""
+
+
+import numpy as np
+import tensorflow as tf
+
+
+class NST:
+    """
+    NST
+    """
+    style_layers = ['block1_conv1', 'block2_conv1',
+                    'block3_conv1', 'block4_conv1', 'block5_conv1']
+    content_layer = 'block5_conv2'
+
+    def __init__(self, style_image, content_image, alpha=1e4, beta=1):
+        if type(style_image) != np.ndarray or style_image.shape[2] != 3:
+            raise TypeError('style_image must be a numpy.ndarray with\
+shape (h, w, 3)')
+        if type(content_image) != np.ndarray or content_image.shape[2] != 3:
+            raise TypeError('content_image must be a numpy.ndarray with shape\
+(h, w, 3)')
+        if alpha <= 0:
+            raise ValueError('alpha must be a non-negative number')
+        if beta <= 0:
+            raise ValueError('beta must be a non-negative number')
+
+        self.style_image = self.scale_image(style_image)
+        self.content_image = self.scale_image(content_image)
+        self.alpha = alpha
+        self.beta = beta
+    
+    @staticmethod
+    def scale_image(image):
+        if type(image) != np.ndarray or image.shape[-1] != 3:
+            raise TypeError("image must be a numpy.ndarray with shape (h, w, 3)")
+
+        h, w = image.shape[:2]
+        max_side = max(h, w)
+        scale_factor = 512 / max_side
+        new_h, new_w = int(scale_factor * h), int(scale_factor * w)
+
+        resized_image = tf.image.resize_images(image, (new_h, new_w))
+        scaled_image = resized_image / 255
+        return scaled_image
+        
