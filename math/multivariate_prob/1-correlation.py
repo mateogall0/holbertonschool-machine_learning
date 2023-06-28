0bfb913c8a65b13d66449040525d66680aac142c
mateogall0 <mateogesede@gmail.com>
2023-06-28 17:02:00 -0300

    done

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -13,6 +13,6 @@ def correlation(C):
     """
     if not isinstance(C, np.ndarray):
         raise TypeError('C must be a numpy.ndarray')
-    if C.shape[0] != C.shape[1]:
+    if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
         raise ValueError('C must be a 2D square matrix')
     return np.corrcoef(C, rowvar=False)
