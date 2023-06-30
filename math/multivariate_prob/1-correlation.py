37a159e651393254eff4d8ad9187c0717f13d548
mateogall0 <mateogesede@gmail.com>
2023-06-30 19:50:23 -0300

    done

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -15,4 +15,5 @@ def correlation(C):
         raise TypeError('C must be a numpy.ndarray')
     if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
         raise ValueError('C must be a 2D square matrix')
-    return np.corrcoef(C, rowvar=False)
+    diagonal = np.diag(1 / np.sqrt(np.diag(C)))
+    return np.matmul(np.matmul(diagonal, C), diagonal)
