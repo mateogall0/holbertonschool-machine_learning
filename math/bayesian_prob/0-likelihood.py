ea5c7cc43a5d122ff1d29ce4ede9a1820e34c814
mateogall0 <mateogesede@gmail.com>
2023-07-04 02:10:03 -0300

    done

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -16,7 +16,7 @@ def likelihood(x, n, P):
     """
     if not isinstance(n, int) or n < 1:
         raise ValueError('n must be a positive integer')
-    if not isinstance(n, int) or x < 0:
+    if not isinstance(x, int) or x < 0:
         raise ValueError(
             'x must be an integer that is greater than or equal to 0'
             )
