a73b0c7e01888748ee382b30be91b4ce4fc82540
mateogall0 <mateogesede@gmail.com>
2023-07-06 02:18:08 -0300

    done

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -11,9 +11,11 @@ def pca(X, ndim):
     """
     Performs PCA on a dataset
     """
-    cov_matrix = np.cov(X, rowvar=False)
+    X_centered = X - np.mean(X, axis=0)
+    cov_matrix = np.cov(X_centered, rowvar=False)
     eigenvalues, eigenvectors = np.linalg.eig(cov_matrix)
     sorted_indices = np.argsort(eigenvalues)[::-1]
     sorted_eigenvectors = eigenvectors[:, sorted_indices]
     selected_eigenvectors = sorted_eigenvectors[:, :ndim]
-    return np.dot(X, selected_eigenvectors)
+
+    return np.dot(X_centered, selected_eigenvectors)
