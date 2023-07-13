3544a0d3865d5d4e455a2399c87d468f3c23cb16
gallit0 <mateogesede@gmail.com>
2023-07-13 20:01:17 -0300

    done

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -11,13 +11,11 @@ def variance(X, C):
     """
     Calculates the total intra-cluster variance for a data set
     """
-    #try:
-    distances = np.linalg.norm(X[:, np.newaxis] - C, axis=2)
-    closest_centroids = np.argmin(distances, axis=1)
-    squared_distances = np.sum((X - C[closest_centroids]) ** 2, axis=1)
-    var = np.sum(squared_distances)
-    return var
-
-    #except Exception:
-    #    return None
-
+    try:
+        distances = np.linalg.norm(X[:, np.newaxis] - C, axis=2)
+        closest_centroids = np.argmin(distances, axis=1)
+        squared_distances = np.sum((X - C[closest_centroids]) ** 2, axis=1)
+        var = np.sum(squared_distances)
+        return var
+    except Exception:
+        pass
