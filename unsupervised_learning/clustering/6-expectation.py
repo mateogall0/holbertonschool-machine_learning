58718cdfea7cbb043da6a4236164d8db321f357a
mateogall0 <mateogesede@gmail.com>
2023-07-16 18:50:46 -0300

    done

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -12,21 +12,17 @@ def expectation(X, pi, m, S):
     """
     Calculates the expectation step in the EM algorithm for a GMM
     """
-    k, = pi.shape
-    n, d = X.shape
+    try:
+        k, = pi.shape
+        n, _ = X.shape
 
-    g = np.zeros((k, n))
-    for i in range(k):
-        diff = X - m[i]
-        inv_S = np.linalg.inv(S[i])
-        exp = -0.5 * np.sum(diff @ inv_S * diff, axis=1)
-        denominator = np.sqrt((2 * np.pi) ** d * np.linalg.det(S[i]))
+        g = np.zeros((k, n))
+        for i in range(k):
+            g[i] = pi[i] * pdf(X, m[i], S[i])
+        marginal = np.sum(g, axis=0)
+        g /= np.sum(g, axis=0)
+        lhood = np.sum(np.log(marginal))
 
-        P = np.maximum(np.exp(exp) / denominator, 1e-300)
-        g[i] = pi[i] * P
-
-    g /= np.sum(g, axis=0)
-
-    lhood = np.sum(np.sum(np.log(g), axis=1))
-
-    return g, lhood
+        return g, lhood
+    except Exception:
+        return None, None
