808695ede948921c2dddb1b458a0a70e72a30de0
mateogall0 <mateogesede@gmail.com>
2023-07-15 22:36:52 -0300

    done

---
+++ unsupervised_learning/clustering/4-initialize.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Initialize
+"""
+
+
+import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+
+
+def initialize(X, k):
+    """
+    Initializes variables for a Gaussian Mixture Model
+    """
+    try:
+        return (np.full((k,), 1/k),
+                kmeans(X, k)[0],
+                np.tile(np.eye(X.shape[1]), (k, 1, 1)))
+    except Exception:
+        return None, None, None
