635c1b81d0fc0d8e8e02da8d92b3c92466a48b1b
mateogall0 <mateogesede@gmail.com>
2023-07-16 22:33:48 -0300

    done

---
+++ unsupervised_learning/clustering/11-gmm.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+GMM
+"""
+
+
+import sklearn.mixture
+
+
+def gmm(X, k):
+    """
+    GMM
+    """
+    gmm = sklearn.mixture.GaussianMixture(n_components=k)
+    gmm.fit(X)
+
+    pi = gmm.weights_
+    m = gmm.means_
+    S = gmm.covariances_
+    clss = gmm.predict(X)
+    bic = gmm.bic(X)
+
+    return pi, m, S, clss, bic
