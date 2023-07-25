48f7982fe137cafbbc82a3bd4ef52dd36f63f2f2
gallit0 <mateogesede@gmail.com>
2023-07-25 14:35:15 -0300

    done

--- unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
@@ -45,21 +45,21 @@ class BayesianOptimization:
         return X_next, EI
 
     def optimize(self, iterations=100):
+        """
+        Optimizes the black-box function
+        """
+        position = []
         for _ in range(iterations):
-            X_next, _ = self.acquisition()
-
-            if X_next in self.gp.X:
-                break
-
+            X_next, ei = self.acquisition()
             Y_next = self.f(X_next)
+            p = np.argmax(ei)
+            if p in position:
+                position.append(p)
+                break
             self.gp.update(X_next, Y_next)
-
-        if self.minimize:
-            idx = np.argmin(self.gp.Y)
+            position.append(p)
+        if self.minimize is True:
+            X_next = np.argmin(self.gp.Y)
         else:
-            idx = np.argmax(self.gp.Y)
-
-        X_opt = self.gp.X[idx]
-        Y_opt = self.gp.Y[idx]
-
-        return X_opt, Y_opt
+            X_next = np.argmax(self.gp.Y)
+        return self.gp.X[X_next], self.gp.Y[X_next]
