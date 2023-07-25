49b19c6b9c90c6b43f3d609ddea83edfa2f7aefe
gallit0 <mateogesede@gmail.com>
2023-07-25 14:26:58 -0300

    done

---
+++ unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""
+Initialize Bayesian Optimization
+"""
+
+
+import numpy as np
+from scipy.stats import norm
+GP = __import__('2-gp').GaussianProcess
+
+
+class BayesianOptimization:
+    """
+    Performs Bayesian optimization on a noiseless 1D Gaussian process
+    """
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1,
+                 sigma_f=1, xsi=0.01, minimize=True):
+        self.f = f
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+        self.X_s = np.linspace(
+            bounds[0], bounds[1], ac_samples).reshape(-1, 1)
+        self.xsi = xsi
+        self.minimize = minimize
+
+    def acquisition(self):
+        """
+        Calculates the next best sample location
+        """
+        mu, sigma = self.gp.predict(self.X_s)
+        mu = mu.reshape(-1, 1)
+        sigma = sigma.reshape(-1, 1)
+
+        if self.minimize:
+            Y_sample = np.min(self.gp.Y)
+        else:
+            Y_sample = np.max(self.gp.Y)
+
+        with np.errstate(divide='warn'):
+            imp = Y_sample - mu - self.xsi
+            Z = imp / sigma
+            ei = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
+            ei[sigma == 0.0] = 0.0
+        X_next = self.X_s[np.argmax(ei)]
+        EI = ei.flatten()
+        return X_next, EI
+
+    def optimize(self, iterations=100):
+        for _ in range(iterations):
+            X_next, _ = self.acquisition()
+
+            if X_next in self.gp.X:
+                break
+
+            Y_next = self.f(X_next)
+            self.gp.update(X_next, Y_next)
+
+        if self.minimize:
+            idx = np.argmin(self.gp.Y)
+        else:
+            idx = np.argmax(self.gp.Y)
+
+        X_opt = self.gp.X[idx]
+        Y_opt = self.gp.Y[idx]
+
+        return X_opt, Y_opt
