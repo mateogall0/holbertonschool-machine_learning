7bb06aa9fc8fb603e9a71dfdb2e691a9233d36ea
gallit0 <mateogesede@gmail.com>
2023-08-03 09:40:39 -0300

    done

---
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+Variational
+"""
+
+
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """
+    Creates a variational autoencoder
+    """
+    encoder_inputs = keras.Input(shape=(input_dims,))
+    x = encoder_inputs
+    for nodes in hidden_layers:
+        x = keras.layers.Dense(nodes, activation='relu')(x)
+    z_mean = keras.layers.Dense(latent_dims)(x)
+    z_log_var = keras.layers.Dense(latent_dims)(x)
+
+    def sampling(args):
+        z_mean, z_log_var = args
+        epsilon = keras.backend.random_normal(
+            shape=(keras.backend.shape(z_mean)[0], latent_dims),
+            mean=0.0, stddev=1.0
+        )
+        return z_mean + keras.backend.exp(0.5 * z_log_var) * epsilon
+
+    z = keras.layers.Lambda(sampling)([z_mean, z_log_var])
+    encoder = keras.Model(
+        encoder_inputs, [z, z_mean, z_log_var], name="encoder"
+    )
+    latent_inputs = keras.Input(shape=(latent_dims,))
+    x = latent_inputs
+    for nodes in reversed(hidden_layers):
+        x = keras.layers.Dense(nodes, activation='relu')(x)
+    decoder_outputs = keras.layers.Dense(input_dims, activation='sigmoid')(x)
+    decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
+
+    autoencoder_outputs = decoder(encoder(encoder_inputs)[0])
+    autoencoder = keras.Model(
+        encoder_inputs, autoencoder_outputs, name="autoencoder"
+    )
+    autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+    return encoder, decoder, autoencoder
