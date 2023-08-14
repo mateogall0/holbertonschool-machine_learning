ddc21433d888632ca93c15f707e048598344557a
gallit0 <mateogesede@gmail.com>
2023-08-13 22:20:14 -0300

    done

--- supervised_learning/RNNs/3-lstm_cell.py
+++ supervised_learning/RNNs/3-lstm_cell.py
@@ -21,23 +21,24 @@ class LSTMCell:
         self.bo = np.zeros((1, h))
         self.by = np.zeros((1, o))
 
-    def sigmoid(self, x):
-        """
-        Sigmoid func
-        """
-        return 1 / (1 + np.exp(-x))
-
     def forward(self, h_prev, c_prev, x_t):
         """
         performs forward propagation for one time step
         """
-        concat_input = np.concatenate((h_prev, x_t), axis=1)
-        ft = self.sigmoid(np.matmul(concat_input, self.Wf) + self.bf)
-        ut = self.sigmoid(np.matmul(concat_input, self.Wu) + self.bu)
-        c_tilde = np.tanh(np.matmul(concat_input, self.Wc) + self.bc)
-        c_next = ft * c_prev + ut * c_tilde
-        ot = self.sigmoid(np.matmul(concat_input, self.Wo) + self.bo)
-        h_next = ot * np.tanh(c_next)
-        y = np.matmul(h_next, self.Wy) + self.by
+        x_concat0 = np.concatenate((h_prev, x_t), axis=1)
+        f = np.matmul(x_concat0, self.Wf) + self.bf
+        f = 1 / (1 + np.exp(-f))
+        u = np.matmul(x_concat0, self.Wu) + self.bu
+        u = 1 / (1 + np.exp(-u))
+        c = np.matmul(x_concat0, self.Wc) + self.bc
+        c = np.tanh(c)
+        o = np.matmul(x_concat0, self.Wo) + self.bo
+        o = 1 / (1 + np.exp(-o))
 
-        return h_next, c_next, y
+        c_next = (u * c) + (f * c_prev)
+        h_t = o * np.tanh(c_next)
+
+        y = np.matmul(h_t, self.Wy) + self.by
+        y = np.exp(y) / np.sum(np.exp(y), axis=1, keepdims=True)
+
+        return h_t, c_next, y
