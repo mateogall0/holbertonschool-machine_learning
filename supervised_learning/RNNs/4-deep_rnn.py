4167a77e92b6a091459951760e6ac7c7dda32cea
gallit0 <mateogesede@gmail.com>
2023-08-13 22:25:03 -0300

    done

--- supervised_learning/RNNs/4-deep_rnn.py
+++ supervised_learning/RNNs/4-deep_rnn.py
@@ -12,26 +12,24 @@ def deep_rnn(rnn_cells, X, h_0):
     Performs forward propagation for a deep RNN
     """
     t, m, i = X.shape
-    ln = len(rnn_cells)
+    l = len(rnn_cells)
+    o = rnn_cells[-1].by.shape[1]
     h = h_0.shape[-1]
 
-    H = np.zeros((t + 1, ln, m, h))
+    H = np.zeros((t + 1, l, m, h))
+    Y = np.zeros((t, m, o))
     H[0] = h_0
 
     Y = np.zeros((t, m, rnn_cells[-1].by.shape[-1]))
-    for t_step in range(t):
-        for layer in range(ln):
-            if t_step == 0:
-                prev_h = h_0[layer]
-            else:
-                prev_h = H[t_step, layer]
-            if layer == 0:
-                new_h, y = rnn_cells[layer].forward(prev_h, X[t_step])
-            else:
-                new_h, y = rnn_cells[
-                    layer].forward(prev_h, H[t_step, layer - 1])
-            H[t_step + 1, layer] = new_h
-            if layer == ln - 1:
-                Y[t_step] = y
+    for step in range(t):
+        h_aux = X[step]
+        for layer in range(len(rnn_cells)):
+            r_cell = rnn_cells[layer]
+            x_t = h_aux
+            h_prev = H[step][layer]
+            h_next, y_next = r_cell.forward(h_prev=h_prev, x_t=x_t)
+            h_aux = h_next
+            H[step + 1][layer] = h_aux
+        Y[step] = y_next
 
-    return H[1:], Y
+    return H, Y
