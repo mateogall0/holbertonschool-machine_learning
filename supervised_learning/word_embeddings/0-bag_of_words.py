b5b7f64a65cffc155c84133308833c9db103f747
gallit0 <mateogesede@gmail.com>
2023-08-17 11:12:16 -0300

    done

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -14,6 +14,7 @@ def bag_of_words(sentences, vocab=None):
     if vocab is None:
         words = [word for sentence in sentences for word in sentence.split()]
         vocab = list(set(words))
+
     word_to_index = {word: i for i, word in enumerate(vocab)}
 
     s = len(sentences)
@@ -25,4 +26,5 @@ def bag_of_words(sentences, vocab=None):
         for word in words:
             if word in word_to_index:
                 embeddings[i, word_to_index[word]] += 1
+
     return embeddings, vocab
