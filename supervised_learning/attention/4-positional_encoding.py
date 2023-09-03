5125a5d6a330a624001b33b8a78040c778b5d771
gallit0 <mateogesede@gmail.com>
2023-09-03 16:28:16 -0300

    done

--- supervised_learning/attention/4-positional_encoding.py
+++ supervised_learning/attention/4-positional_encoding.py
@@ -5,6 +5,9 @@ import numpy as np
 
 
 def positional_encoding(max_seq_len, dm):
+    """
+    Calculates the positional encoding for a transformer
+    """
     position = np.arange(max_seq_len)[:, np.newaxis]
     div_term = np.exp(np.arange(0, dm, 2) * -(np.log(10000.0) / dm))
     pos_enc = np.zeros((max_seq_len, dm))
