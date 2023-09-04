b696ce5d4ef24e19ed0e2909131b409b8cfba561
gallit0 <mateogesede@gmail.com>
2023-09-03 21:42:35 -0300

    done

--- supervised_learning/attention/10-transformer_decoder.py
+++ supervised_learning/attention/10-transformer_decoder.py
@@ -15,15 +15,15 @@ class Decoder(tf.keras.layers.Layer):
     """
     def __init__(self, N, dm, h, hidden, target_vocab,
                  max_seq_len, drop_rate=0.1):
-        super(Decoder, self).__init__()
+        super().__init__()
         self.N = N
         self.dm = dm
         self.embedding = tf.keras.layers.Embedding(
-            input_dim=target_vocab, output_dim=dm)
+            target_vocab, dm)
         self.positional_encoding = positional_encoding(max_seq_len, dm)
         self.blocks = [DecoderBlock(dm, h, hidden, drop_rate)
                        for _ in range(N)]
-        self.dropout = tf.keras.layers.Dropout(rate=drop_rate)
+        self.dropout = tf.keras.layers.Dropout(drop_rate)
 
     def call(self, x, encoder_output, training, look_ahead_mask,
              padding_mask):
