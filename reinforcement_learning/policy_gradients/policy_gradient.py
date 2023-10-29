3808fac844bb27a71a74d6843a18e22a680bfc45
mateogall0 <mateogesede@gmail.com>
2023-10-29 18:41:32 -0300

    done

--- reinforcement_learning/policy_gradients/policy_gradient.py
+++ reinforcement_learning/policy_gradients/policy_gradient.py
@@ -8,3 +8,12 @@ def policy(matrix, weight):
                                                      axis=1, keepdims=True)
 
     return policy_matrix
+
+def policy_gradient(state, weight):
+    policy_probabilities = policy(state, weight)
+
+    action = np.random.choice(np.arange(policy_probabilities.shape[1]), p=policy_probabilities[0])
+
+    gradient = state[0] - np.dot(state[0], policy_probabilities[0])
+
+    return action, gradient
