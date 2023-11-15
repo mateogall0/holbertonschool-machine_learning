c1536c0dcf323e71e73e7e435f59d9f94193d883
gallit0 <mateogesede@gmail.com>
2023-11-15 11:10:17 -0300

    done

--- pipeline/databases/11-rating_shows.sql
+++ pipeline/databases/11-rating_shows.sql
@@ -1,6 +1,7 @@
 -- task 11
-SELECT tv_shows.title, SUM(ratings.rating) AS rating_sum
-FROM tv_shows
-JOIN ratings ON tv_shows.id = ratings.show_id
-GROUP BY tv_shows.id
-ORDER BY rating_sum DESC;
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+FROM tv_show_ratings
+JOIN tv_shows
+ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY tv_shows.title
+ORDER BY rating DESC;
