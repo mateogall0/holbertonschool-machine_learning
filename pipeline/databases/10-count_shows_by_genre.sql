69bc9795fe8ba66d9294bbed538c3490798d492e
gallit0 <mateogesede@gmail.com>
2023-11-15 11:06:16 -0300

    done

--- pipeline/databases/10-count_shows_by_genre.sql
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -1,14 +1,7 @@
 -- task 10
-SELECT
-  tv_genres.genre_name AS genre,
-  COUNT(tv_shows.id) AS number_of_shows
-FROM
-  tv_genres
-JOIN
-  tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN
-  tv_shows ON tv_show_genres.show_id = tv_shows.id
-GROUP BY
-  tv_genres.id
-ORDER BY
-  number_of_shows DESC;
+SELECT tg.name AS genre, COUNT(g.genre_id) AS number_of_shows
+FROM tv_show_genres AS g
+JOIN tv_genres AS tg
+ON tg.id = g.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
