7b345c6f656d24e146ea9af633b0569ecf7b3451
mateogall0 <mateogesede@gmail.com>
2024-06-24 10:25:52 -0300

    Fix pycodestyle

--- reinforcement_learning/q_learning/2-epsilon_greedy.py
+++ reinforcement_learning/q_learning/2-epsilon_greedy.py
@@ -8,5 +8,5 @@ def epsilon_greedy(Q, state, epsilon):
     """
     p = np.random.uniform(0, 1)
     if p > epsilon:
-        return np.argmax(Q[state,:])
+        return np.argmax(Q[state, :])
     return np.random.randint(0, Q.shape[1])
